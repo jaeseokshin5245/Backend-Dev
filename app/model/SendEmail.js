1aecebfbaf9ec6404133fe2c45f520f0730e3e76
Hwang Eunji <a01065107174@gmail.com>
2024-06-18 02:12:48 +0900

    이메일 코드는 다 써놨는데 덜 완성했는데 머리가 일을 안해...

--- app/model/SendEmail.js
+++ app/model/SendEmail.js
@@ -15,8 +15,31 @@ const smtpTransport= nodemailer.createTransport({
 class SendEmail{
     static checkemail(){
         const handler =async(
-            // req:NextApiRequest,
-            // res:NextApiResponse
-        )
+            req:NextApiRequest,
+            res:NextApiResponse
+        ) =>{
+            const {email}=req.body;
+            const payload=Math.floor(100000 + Math.random()*9000000)+'';
+
+            if(email){
+                const mailOptions={
+                    from: process.env.NODE_MAILER_ID,
+                    to :email,
+                    subject: '~~',
+                    html:'<strong>http 들어가나봐.</strong>',
+                }
+                await smtpTransport.sendMail(mailOptions,(error, responses)=>{
+                    if(error){
+                        res.statub(400).json({success:false})
+                    }else{
+                        res.statuc(200).json({success:true})
+                    }
+                    smtpTransport.close();
+                })
+            }
+            return res.status(200).json({success:true});
+        }
     }
 }
+
+export default handler;
